c11d1820babeb0691e355939b2616d6ecac47c19
akdedeimtisal <akdedeimtisall@gmail.com>
2021-12-12 22:51:28 +0300

    authentication guards added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import {NgModule} from '@angular/core';
 import {Router, RouterModule, Routes} from '@angular/router';
 import {HomeComponent} from "./guest/home/home.component";
 import {LoginComponent} from "./guest/login/login.component";
@@ -7,6 +7,8 @@ import {ProfileComponent} from "./user/profile/profile.component";
 import {AdminComponent} from "./admin/admin/admin.component";
 import {NotFoundComponent} from "./error/not-found/not-found.component";
 import {UnauthorizedComponent} from "./error/unauthorized/unauthorized.component";
+import {AuthGuard} from "./guards/auth.guard";
+import {Role} from "./models/role.enum";
 
 const routes: Routes = [
   //sadece boş component olduğunda home yoluna yönlendir.
@@ -15,8 +17,17 @@ const routes: Routes = [
   {path: 'home', component: HomeComponent},
   {path: 'login', component: LoginComponent},
   {path: 'register', component: RegisterComponent},
-  {path: 'profile', component: ProfileComponent},
-  {path: 'admin', component: AdminComponent},
+  { path: 'profile',
+    component: ProfileComponent,
+    canActivate: [AuthGuard],
+    data: { roles: [Role.ADMIN, Role.USER]}
+  },
+
+  { path: 'admin',
+    component: AdminComponent,
+    canActivate: [AuthGuard],
+    data: {roles: [Role.ADMIN]}
+  },
   {path: '404', component: NotFoundComponent},
   {path: '401', component: UnauthorizedComponent}
 
