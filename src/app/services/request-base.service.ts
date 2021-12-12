4e2e3d3e17ef81e15fe15cde4104a81795735b7e
akdedeimtisal <akdedeimtisall@gmail.com>
2021-12-12 23:08:02 +0300

    book service added

--- src/app/services/request-base.service.ts
+++ src/app/services/request-base.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
+import {User} from "../models/user.model";
+import {AuthenticationService} from "./authentication.service";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+
 
 @Injectable({
   providedIn: 'root'
 })
-export class RequestBaseService {
+export abstract class RequestBaseService {
 
-  constructor() { }
+  protected currentUser: User = new User;
+
+  protected constructor(protected authenticationService: AuthenticationService, protected http: HttpClient) {
+    this.authenticationService.currentUser.subscribe(data => {
+      this.currentUser = data;
+    });
+
+
+  }
+
+  get getHeders(): HttpHeaders {
+    return new HttpHeaders(
+      {
+        authorization: 'Bearer' + this.currentUser?.token,
+        "Content-Type": "application/json; charset-UTF-8"
+      }
+    );
+  }
+
 }
